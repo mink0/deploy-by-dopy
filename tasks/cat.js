8c0b53ee2c96c81b7b899158d56ed07b627bb011
mmekhanov <mikhail.mekhanov@eurochem.ru>
2017-05-28 20:42:45 +0300

    Rewrite cat task

--- tasks/cat.js
+++ tasks/cat.js
@@ -1,27 +1,57 @@
 const dopy = global.dopy;
+const get = require('lodash.get');
 
-exports.command = 'cat [file]';
+exports.command = 'cat [target] [file]';
 
 exports.desc = 'Print file from remote server';
 
 exports.builder = (yargs) => {
   if (!dopy.config.env.config.remote) return;
 
-  let targets = dopy.config.env.config.remote.cat;
+  let targets = dopy.config.env.config.targets;
 
-  if (!targets || typeof targets !== 'object') return;
+  if (targets) {
+    yargs.demand(1);
+    for (let target in targets) {
+      let descr = `${target} cat path: ${JSON.stringify(targets[target].remote.cat)}`;
+      yargs.command(target, descr, () => {
+        let cats = targets[target].remote.cat;
 
-  for (let target in targets) {
-    yargs.command(target, `path: ${targets[target]}`);
+        for (let cmd in cats) {
+          yargs.command(cmd, cats[cmd]);
+        }
+      });
+    }
+  }
+
+  // global targets
+  let gtargets = dopy.config.env.config.remote.cat;
+
+  for (let target in gtargets) {
+    yargs.command(target, `cat path: ${JSON.stringify(gtargets[target])}`);
   }
 };
 
 exports.task = (env, argv, taskCb) => {
-  let files = env.config.remote.cat;
+  let file = argv.file;
+  let target = argv.target;
+  let files;
 
-  if (!files) return taskCb('no cat files configured for ' + env.file);
+  if (!file) {
+    file = target;
+    target = null;
+    files = env.config.remote.cat;
+  }
 
-  let path = (typeof files === 'object') ? files[argv.file || 'config'] : files;
+  if (target)
+    if (get(env.config.targets[target], 'remote.cat'))
+      files = env.config.targets[target].remote.cat;
 
-  env.remote(`cat ${path}`, { verbose:true }, taskCb);
+  let cmd;
+  if (files && files[file])
+    cmd = files[file];
+  else
+    cmd = file;
+
+  env.remote(`cat ${cmd}`, { verbose:true }, taskCb);
 };
